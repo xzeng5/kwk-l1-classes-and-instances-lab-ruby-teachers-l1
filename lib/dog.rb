eccbf19d4991570d49d77ba777e2548b9dac8c6f
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-25 22:55:41 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+class Dog
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
+
+fido = Dog.new("fido")
+
+snoopy = Dog.new("snoopy")
+
+lassie = Dog.new("lassie")
